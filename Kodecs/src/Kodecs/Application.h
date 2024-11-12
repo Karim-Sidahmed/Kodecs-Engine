af81b3ae5478806f62f6ff84c22b81f57fbaf0cc
K4R1M-K <karim.vwa@gmail.com>
2024-11-12 15:01:54 +0100

    Added Comments for better Understanding

--- Kodecs/src/Kodecs/Application.h
+++ Kodecs/src/Kodecs/Application.h
@@ -1,3 +1,4 @@
+// Application.h file
 #pragma once
 
 #include "Core.h"
@@ -7,14 +8,16 @@ namespace Kodecs {
 	class KODECS_API Application {
 
 	public:
+		// Constructor and destructor
 		Application();
 		virtual ~Application();
 
+		// Run method that starts the main loop of the application
 		void Run();
 	};
 
-	//To be defined in our client
+	// Declaration for a function to be defined in the client application.
+	// This function will create an Application object for the client.
 	Application* CreateApplication();
 
 }
-
